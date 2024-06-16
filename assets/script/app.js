883b73becb2251b02a66d40c0ffe50e0dfb2c83b
Aaron Watts <aaron.watts@murena.io>
2024-06-16 13:58:39 +0100

    use iframe if api fails

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,6 +1,3 @@
-const devData = { "data": { "holidayHours": [{ "start": "2024-07-24T23:00:00.000Z", "end": "2024-08-30T23:00:00.000Z", "title": "Summer Holiday", "description": "Open Hours 12-6pm (except Bank Holidays)" }, { "start": "2024-08-25T23:00:00.000Z", "end": "2024-08-26T23:00:00.000Z", "title": "Bank holiday", "description": "Closed" }], "events": [{ "start": "2024-07-06T16:00:00.000Z", "end": "2024-07-06T19:00:00.000Z", "title": "CLT event", "description": "" }] } }
-// const devData = {'data':{'holidayHours': [], 'events': []}};
-
 const getEvents = async () => {
     const res = await fetch(
         'https://script.google.com/macros/s/AKfycbwy-Jfw-II_FcG0bhNqKm8KtP-e48g5_CFSBv-nOBbvUVSFftRKrfgceIqYTRnj5W9v/exec',
@@ -82,28 +79,35 @@ const buildEvent = event => {
 };
 
 const populateLists = async () => {
-    // const data = await getEvents();
-    const data = devData;
-
-    const { events, holidayHours } = data.data;
     const eventList = document.querySelector('#events--list');
     const termDatesList = document.querySelector('#term-dates--list');
 
-    if (events.length) {
-        events.forEach(event => {
-            eventList.appendChild(buildEvent(event));
-        });
-    } else {
-        eventList.innerText = 'No events information available'
-    };
+    try {
+        const data = await getEvents();
+        const { events, holidayHours } = data.data;
+
+        if (events.length) {
+            events.forEach(event => {
+                eventList.appendChild(buildEvent(event));
+            });
+        } else {
+            eventList.innerText = 'No events information available'
+        };
+        
+        if (holidayHours.length) {
+            holidayHours.forEach(event => {
+                termDatesList.appendChild(buildEvent(event));
+            });
+        } else {
+            termDatesList.innerText = 'No holiday information available';
+        };
+    } catch (e) {
+        const eventFrame = `<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FLondon&bgcolor=%23ffffff&showTitle=0&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=AGENDA&hl=en_GB&src=NjdhM2RhYmRiNjkxMjNjNzg2MzkyMWYwMTYwYjE1MjdhZWFmOGI0OWMwZDEyOWI1N2ZmMTM4OTMxYTdjNTU5MUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23795548" style="border:solid 1px #777" width="90%" height="400" frameborder="0" scrolling="no"></iframe>`;
+        const termDateFrame = `<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FLondon&bgcolor=%23ffffff&showTitle=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=MONTH&hl=en_GB&src=N2FwZGFzcTJ1ODJlZmNoa2pmdHRxbG82Y2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23AD1457" style="border:solid 1px #777" width="90%" height="400" frameborder="0" scrolling="no"></iframe>`;
+        eventList.innerHTML = eventFrame;
+        termDatesList.innerHTML = termDateFrame;
+    }
     
-    if (holidayHours.length) {
-        holidayHours.forEach(event => {
-            termDatesList.appendChild(buildEvent(event));
-        });
-    } else {
-        termDatesList.innerText = 'No holiday information available';
-    };
     
 };
 
