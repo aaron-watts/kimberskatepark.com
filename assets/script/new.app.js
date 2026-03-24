8d9729b3e1769cfb21c12da100cd0e37a9eebceb
Aaron Watts <hello@aaronwatts.dev>
2026-03-24 14:32:50 +0000

    dialog on api fail

--- assets/script/new.app.js
+++ assets/script/new.app.js
@@ -2,19 +2,35 @@
 
 class App {
     constructor(data) {
+        this.disclaimer = document.getElementById('disclaimer');
+        this.confirmBtn = this.disclaimer.querySelector('#confirmBtn');
+        this.confirmBtn.addEventListener('click', evt => {
+            evt.preventDefault();
+            this.buildIframes();
+        });
         this.data = data;
+        if (this.data == null) this.openDialog(this.disclaimer);
+    }
+
+    openDialog(dialog){
+        dialog.showModal();
+    }
+
+    buildIframes() {
+        console.log('build iframes');
     }
 
     static async getEvents(route) {
         const res = await fetch(route);
-        console.log(res.status);
+        let data;
+        
         if (res.status == 200) {
-            const data = await res.json();
-            return data;
+            data = await res.json();
         } else {
-            const data = null;
-            return data;
+            data = null;
         }
+
+        return data;
     }
 }
 
@@ -25,5 +41,6 @@ async function init() {
 
     const data = await App.getEvents('/.netlify/functions/api');
     const app = new App(data);
-    console.log(app.data);
+
+    
 }
