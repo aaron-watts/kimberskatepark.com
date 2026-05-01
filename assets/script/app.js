f2bd5b3f6f2063ab4bf3778cf80911afa1e29e8a
Aaron Watts <hello@aaronwatts.dev>
2026-05-01 12:32:07 +0100

    redesign home

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,13 +1,85 @@
-const getEvents = async () => {
-    const res = await fetch('/.netlify/functions/api');
+'use strict'
 
-    const data = await res.json();
+class App {
+    constructor(data) {
+        this.scheduleEl = document.querySelector('#schedule-list');
+        this.userMsg = document.querySelector('#user-msg');
+        this.data = data;
+        if (this.data) {
+            this.schedule = [...this.data.holidayHours, ...this.data.events];
+            this.schedule.sort((a, b) => new Date(a.start) - new Date(b.start));
+        }
+    }
 
-    return data;
-}
+    dataError() {
+        this.userMsg.innerText = 'Something went wrong while fetching the Schedule.';
+        this.userMsg.innerText += ' Please try again later.';
+    }
 
-const formatDate = (start, end) => {
-    const isAllDay = (start, end) => {
+    populateSchedule() {
+        for (let event of this.schedule) {
+            const liEl = document.createElement('li');
+            liEl.appendChild(this.buildArticle(event));
+            this.scheduleEl.appendChild(liEl);
+        }
+
+        this.userMsg.classList.add('hidden');
+    }
+
+    buildArticle(event) {
+        const { start, end, title, description } = event;
+        const articleEl = document.createElement('article');
+        const timeEl = document.createElement('h3');
+        const titleEl = document.createElement('h4');
+        const pEl = document.createElement('p');
+        timeEl.innerHTML = this.formatDateStr(start, end);
+        titleEl.innerText = title;
+        pEl.innerText = description;
+        articleEl.appendChild(timeEl);
+        articleEl.appendChild(titleEl);
+        articleEl.appendChild(pEl);
+        return articleEl;
+    }
+
+    formatDateStr(eventStart, eventEnd) {
+        const start = this.dateData(eventStart);
+        const end = this.dateData(eventEnd);
+        const isAllDay = this.isAllDay(start.date, end.date);
+        const isOneDay = this.isOneDay(start.date, end.date);
+        let dateString = '';
+        if (!isAllDay && isOneDay) {
+            dateString += `<time datetime="${start.yy}-${start.mm}-${start.dd} ${start.HH}:${start.MM}">`
+            dateString += `${start.HH}:${start.MM}</time> - `;
+            dateString += `<time datetime="${end.yy}-${end.mm}-${end.dd} ${end.HH}:${end.MM}">`;
+            dateString += `${end.HH}:${end.MM}</time> `
+        }
+        dateString += `<time datetime="${start.yy}-${start.mm}-${start.dd}">${start.date.toDateString()}</time>`;
+        if (!isOneDay) {
+            end.date.setDate(end.date.getDate() - 1);
+            const updateEnd = this.dateData(end.date)
+            dateString += ` - <time datetime="${updateEnd.yy}-${updateEnd.mm}-${updateEnd.dd}">`
+            dateString += `${updateEnd.date.toDateString()}</time>`;
+        }
+        return dateString;
+    }
+
+    dateData(date) {
+        const data = {
+            date: new Date(date)
+        };
+        data.yy = data.date.getFullYear();
+        data.mm = this.doubleDigit(data.date.getMonth() + 1);
+        data.dd = this.doubleDigit(data.date.getDate());
+        data.HH = this.doubleDigit(data.date.getHours());
+        data.MM = this.doubleDigit(data.date.getMinutes());
+        return data;
+    }
+
+    doubleDigit(i) {
+        return i < 10 ? `0${i}` : i;
+    }
+
+    isAllDay(start, end) {
         if (
             !start.getHours()
             && !start.getMinutes()
@@ -19,94 +91,39 @@ const formatDate = (start, end) => {
         return false;
     }
 
-    const isOneDay = (start, end) => {
+    isOneDay(start, end) {
         const diffTime = Math.abs(start - end);
         const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
         if (diffDays <= 1) return true;
         return false;
     }
 
-    const dateElement = document.createElement('div');
-    let dateString = '';
-
-    if (!isAllDay(start, end) && isOneDay(start, end)) {
-        const startHH = start.getHours();
-        const startMM = start.getMinutes();
-        const endHH = end.getHours();
-        const endMM = end.getMinutes();
-
-        dateString += `${startHH}:${
-            startMM.toString().length < 2 ? '0' + startMM : startMM
-        } - ${endHH}:${
-            endMM.toString().length < 2 ? '0' + endMM : endMM
-        }, `;
-    }
-
-    dateString += `${start.toDateString()}`;
-
-    if (!isOneDay(start, end)) {
-        end.setDate(end.getDate() - 1);
-        dateString += ` - ${end.toDateString()}`;
-    }
-
-    return dateString;
-};
-
-const buildEvent = event => {
-    const { start, end, title, description } = event;
-
-    const eventItem = document.createElement('div');
-    eventItem.classList.add('entry');
-    const titleItem = document.createElement('h4');
-    titleItem.innerText = title;
-
-    const startDate = new Date(start)
-    const endDate = new Date(end);
-    
-    const dateElement = document.createElement('h3');
-    dateElement.innerText = formatDate(startDate, endDate);
-
-    const descriptionItem = document.createElement('p');
-    descriptionItem.innerHTML = description;
-    const anchors = descriptionItem.querySelectorAll('a:not([target="_blank"])');
-    if (anchors.length) anchors.forEach(a => a.target = '_blank');
-
-    eventItem.appendChild(dateElement);
-    eventItem.appendChild(titleItem);
-    eventItem.appendChild(descriptionItem);
-
-    return eventItem;
-};
-
-const populateLists = async () => {
-    const scheduleList = document.querySelector('#schedule--list');
-    const eventList = document.querySelector('#events--list');
-    const termDatesList = document.querySelector('#term-dates--list');
-    const loaderWidgets = document.querySelectorAll('.loader');
-
-    try {
-        const data = await getEvents();
-        const schedule = [...data.holidayHours, ...data.events];
-        schedule.sort((a, b) => new Date(a.start) - new Date(b.start));
-
-        if (schedule.length) {
-            schedule.forEach(event => {
-                scheduleList.appendChild(buildEvent(event));
-            });
+    static async getEvents(route) {
+        const res = await fetch(route);
+        let data;
+        if (res.status == 200) {
+            data = await res.json();
         } else {
-            scheduleList.innerText = 'No events information available'
-        };
-    } catch (e) {
-        const eventFrame = `<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FLondon&bgcolor=%23ffffff&showTitle=0&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=AGENDA&hl=en_GB&src=NjdhM2RhYmRiNjkxMjNjNzg2MzkyMWYwMTYwYjE1MjdhZWFmOGI0OWMwZDEyOWI1N2ZmMTM4OTMxYTdjNTU5MUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23795548" style="border:solid 1px #777" width="90%" height="400" frameborder="0" scrolling="no"></iframe>`;
-        const termDateFrame = `<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FLondon&bgcolor=%23ffffff&showTitle=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=MONTH&hl=en_GB&src=N2FwZGFzcTJ1ODJlZmNoa2pmdHRxbG82Y2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23AD1457" style="border:solid 1px #777" width="90%" height="400" frameborder="0" scrolling="no"></iframe>`;
-        eventList.innerHTML = eventFrame;
-        termDatesList.innerHTML = termDateFrame;
-        document.querySelector('#schedule').classList.toggle('hidden');
-        document.querySelector('#events').classList.toggle('hidden');
-        document.querySelector('#term-dates').classList.toggle('hidden');
+            data = null;
+        }
+        return data;
     }
-    
-    loaderWidgets.forEach(i => i.classList.add('hidden'))
-};
 
-document.addEventListener('DOMContentLoaded', populateLists)
+    static detectScript() {
+        const noscript = document.querySelectorAll('.noscript');
+        for (const el of noscript) {
+            el.classList.remove('noscript');
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+async function init() {
+    console.log('Initialise App');
+    App.detectScript();
+    const data = await App.getEvents('/.netlify/functions/api');
+    const app = new App(data);
+    if (app.data) app.populateSchedule();
+    else app.dataError();
+}
